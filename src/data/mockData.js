55eb0d0bc389d2562330009c6d4df4855176f127
Jakmis <jakmis007@gmail.com>
2023-02-01 22:44:59 +0100

    add theme

---
+++ src/data/mockData.js
@@ -0,0 +1,17 @@
+export const mockData = [
+    {
+        id: 1,
+        name: "Jakub Hudymač",
+        email: "jakmis007@gmail.com",
+        age: 19,
+        access: "admin"
+    },
+    {   
+        id: 2,
+        name: "Lukáš Hanke",
+        email: "lukashanke@seznam.cz",
+        age: 20,
+        access: "user"
+    }
+
+]
